bf68facbd219916b0cc82466c8d6965f90047460
SparklySkittles <saltandskittles@gmail.com>
2024-02-03 14:24:32 -0800

    Added Tooltip

--- src/main/java/net/sparklyskittles/wackyweapons/item/SlimeHammerSword.java
+++ src/main/java/net/sparklyskittles/wackyweapons/item/SlimeHammerSword.java
@@ -1,18 +1,18 @@
 package net.sparklyskittles.wackyweapons.item;
 
-import net.minecraft.enchantment.Enchantment;
-import net.minecraft.enchantment.EnchantmentHelper;
-import net.minecraft.enchantment.Enchantments;
-import net.minecraft.enchantment.KnockbackEnchantment;
+import net.minecraft.client.item.TooltipContext;
 import net.minecraft.entity.LivingEntity;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.ItemUsageContext;
 import net.minecraft.item.SwordItem;
 import net.minecraft.item.ToolMaterial;
+import net.minecraft.text.Text;
 import net.minecraft.util.ActionResult;
 import net.minecraft.util.math.Vec3d;
-import org.apache.commons.lang3.ObjectUtils;
+import net.minecraft.world.World;
+import org.jetbrains.annotations.Nullable;
 
+import java.util.List;
 import java.util.Objects;
 
 public class SlimeHammerSword extends SwordItem {
@@ -35,5 +35,10 @@ public class SlimeHammerSword extends SwordItem {
     public boolean postHit(ItemStack stack, LivingEntity target, LivingEntity attacker) {
         target.takeKnockback(4, attacker.getX() - target.getX(), attacker.getZ() - target.getZ());
         return super.postHit(stack, target, attacker);
+    }
+    @Override
+    public void appendTooltip(ItemStack stack, @Nullable World world, List<Text> tooltip, TooltipContext context) {
+        tooltip.add(Text.translatable("wackyweapons.slime_hammer.tooltip"));
+        super.appendTooltip(stack, world, tooltip, context);
     }
 }
